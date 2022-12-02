de8aed7056a382dadf867b648d99147b7feac9d2
ivaniuss <ivan.emi94@gmail.com>
2022-12-01 19:00:36 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 80rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+</style>
---
+++ src/routes/Counter.svelte
@@ -0,0 +1,50 @@
+<script>
+	let value = ""
+	let output = ""
+	function handleSubmit () {
+		output = value
+	}
+</script>
+
+<div class="grammar">
+	<div>
+		<h3>Input</h3>
+		<textarea bind:value></textarea>
+	</div>
+	<div class="btn">
+		<button on:click={handleSubmit}>send</button>
+	</div>
+	<div>
+		<h3>Output</h3>
+		<textarea readonly>{output}</textarea>
+	</div>
+</div>
+
+<style>
+	.grammar {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: space-around;
+		gap: 5vw;
+	}
+	textarea {
+		width: 33vw; 
+		height: 70vh;
+	}
+	.btn {
+		display: flex;
+		align-items: center;		
+	}
+
+	button {
+		background-color: black;
+	}
+
+
+@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
